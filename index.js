2f4db7f836994ceaf27b0a11038395f4cbfd8d32
saifuddeen <saifunek@gmail.com>
2024-11-30 05:34:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+//get element by id 
+let Element = document.getElementbyId ("heading")
+console.log(element);
+
+//get elements by class name  
+letelement = document.getelementbyclassname("containor")
+console.log (containor);
+
+let element= document.getElementsByClassName("section")
+console.log(section)
+
+//get element by tag name 
+letElement = Document.getelementsbytagname("p")
+console.log(element);
+
+//queryselector 
+const firstbutton  = Document.queryselector(".btn")
+console.log(firstbutton);
+
+const firstsection= Document.querySelector(".section")
+console.log(firstsection);
+
+//queryselectoral
+const allbuttons = document.querySelectorAll(".btn")
+console.log(allbuttons);
+
+
+
